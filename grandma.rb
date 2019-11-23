9ac797d302e2052f5eb830f2b8eec43d470b0af2
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-11-23 01:24:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,5 +18,7 @@ def speak_to_grandma(input)
     return "HUH?! SPEAK UP, SONNY!"
   elsif "#{input}" == "#{input}".upcase
     return "NO, NOT SINCE 1938!"
+  else
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
